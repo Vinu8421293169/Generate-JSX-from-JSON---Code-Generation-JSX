cc85fff38045d809e32602c064d0d29f69ade9f4
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-10-22 20:13:57 +0530

    initial changes

---
+++ src/components/circle.js
@@ -0,0 +1,9 @@
+import Shape from "./shape";
+
+export default class Circle extends Shape {
+  construction(color) {
+    super(color);
+  }
+  drawShape = function () {};
+  calculateArea = function () {};
+}
---
+++ src/components/shape.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Circle {
+  color = null;
+
+  constructor(color) {
+    this.color = color;
+  }
+
+  calculateArea = function () {};
+  drawShape = function () {};
+}
