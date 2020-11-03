3d2da0d0f8a6f44192ef8ee22e020dae74d041e8
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-03 11:21:48 +0530

    delete circle.js

--- src/components/circle.js
+++
@@ -1,9 +0,0 @@
-import Shape from "./shape";
-
-export default class Circle extends Shape {
-  construction(color) {
-    super(color);
-  }
-  drawShape = function () {};
-  calculateArea = function () {};
-}
